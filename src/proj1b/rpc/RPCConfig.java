369eabd89c97b5f3a017586a95228ac152647a96
girikuncoro <girikuncoro@gmail.com>
2016-04-02 12:47:08 -0400

    Added RPC config constatns

---
+++ src/proj1b/rpc/RPCConfig.java
@@ -0,0 +1,6 @@
+package proj1b.rpc;
+
+public class RPCConfig {
+	public static final int PORT = 5300;
+	public static final int MAX_PACKET_LENGTH = 512;
+}
--- src/proj1b/rpc/RPCServer.java
+++ src/proj1b/rpc/RPCServer.java
@@ -1,5 +1,13 @@
 package proj1b.rpc;
 
-public class RPCServer {
+import java.net.DatagramSocket;
 
+public class RPCServer implements Runnable {
+	DatagramSocket rpcSocket = new DatagramSocket(RPCConfig.PORT);
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		
+	}
 }
