fa239c2f501c7c99d433a66b1a5d4d0af3e17c0f
AaronZang <sz428@cornell.edu>
2016-04-02 16:16:28 -0400

    add rpc delimiter

--- src/proj1b/rpc/RPCConfig.java
+++ src/proj1b/rpc/RPCConfig.java
@@ -3,4 +3,9 @@ package proj1b.rpc;
 public class RPCConfig {
 	public static final int PORT = 5300;
 	public static final int MAX_PACKET_LENGTH = 512;
+	public static final int NO_OP_CODE = 0;
+	public static final int READ_CODE = 1;
+	public static final int WRITE_CODE = 2;
+	
+	public static final String RPC_DELIMITER = "_";
 }
