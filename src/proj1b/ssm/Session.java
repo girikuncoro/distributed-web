5ff8125439e68e29129f29a2bf9cd171b9f96321
AaronZang <sz428@cornell.edu>
2016-04-02 18:31:42 -0400

    add SSM package

---
+++ src/proj1b/ssm/Session.java
@@ -0,0 +1,51 @@
+package proj1b.ssm;
+
+import java.sql.Timestamp;
+import java.util.*;
+
+public class Session {
+	private String sessionID;
+	private int versionNumber;
+	private String message;
+	private Timestamp creationTime;
+	private Timestamp expirationTime;
+	private List<String> locationData;
+
+	public Session(String sessionID, int versionNumber, String message, Timestamp creationTime,
+			Timestamp expirationTime, List<String> locationData) {
+		this.sessionID = sessionID;
+		this.versionNumber = versionNumber;
+		this.message = message;
+		this.creationTime = creationTime;
+		this.expirationTime = expirationTime;
+		this.locationData = locationData;
+	}
+
+	public Session(String sessionID, List<String> locationData) {
+		this(sessionID, 0, "", null, null, locationData);
+
+		Date date = new Date();
+		this.creationTime = new Timestamp(date.getTime());
+		this.expirationTime = new Timestamp(creationTime.getTime() + (SessionManager.getTimeToLive() * 1000));
+	}
+
+	public Session(String sessionID, List<String> locationData, String message) {
+		this(sessionID, locationData);
+		this.message = message;
+	}
+
+	public String encode() {
+		String locationDataString = String.join(SessionManager.SESSION_DELIMITER, locationData);
+		return String.join(SessionManager.SESSION_DELIMITER, sessionID, String.valueOf(versionNumber), message,
+				creationTime.toString(), expirationTime.toString(), locationDataString);
+	}
+
+	public static Session decode(String encodedSession) {
+		List<String> fields = Arrays.asList(encodedSession.split(SessionManager.SESSION_DELIMITER));
+		List<String> locationData = new ArrayList<String>();
+		locationData.addAll(fields.subList(5, fields.size()));
+
+		return new Session(fields.get(0), Integer.parseInt(fields.get(1)), fields.get(2),
+				Timestamp.valueOf(fields.get(3)), Timestamp.valueOf(fields.get(4)), locationData);
+	}
+}
---
+++ src/proj1b/ssm/SessionManager.java
@@ -0,0 +1,38 @@
+package proj1b.ssm;
+
+import java.util.Collection;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class SessionManager {
+//	private static Map<String, Session> sessionDataTable = new ConcurrentHashMap<String, Session>();
+	
+	private static final int TIME_TO_LIVE = 60;
+	public static final String SESSION_DELIMITER = "#";
+	
+	public static int getTimeToLive() {
+		return TIME_TO_LIVE;
+	}
+//	
+//	public static void addToTable(Session session) {
+//		sessionDataTable.put(session.getSessionID(), session);
+//	}
+//	
+//	public static void removeFromTable(Session session) {
+//		try {
+//			sessionDataTable.remove(session.getSessionID());
+//		}
+//		catch(Exception e) {
+//			System.out.println("Session: " + session.getSessionID() + " has already been removed.");
+//		}
+//	}
+//	
+//	public static Session getSession(String sessionName) {
+//		if(!sessionDataTable.containsKey(sessionName)) return null;
+//		return sessionDataTable.get(sessionName);
+//	}
+//	
+//	public static Collection<Session> getTableValues() {
+//		return sessionDataTable.values();
+//	}
+}
