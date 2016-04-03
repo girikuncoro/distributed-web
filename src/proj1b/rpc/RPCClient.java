09c981c15ebea53827a06a94aa573e53c369e229
AaronZang <sz428@cornell.edu>
2016-04-02 21:48:47 -0400

    finish sessionRead function in RPCClient

--- src/proj1b/rpc/RPCClient.java
+++ src/proj1b/rpc/RPCClient.java
@@ -1,5 +1,58 @@
 package proj1b.rpc;
 
-public class RPCClient {
+import java.util.List;
 
+import proj1b.ssm.Session;
+
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+import java.io.IOException;
+import java.net.SocketTimeoutException;
+
+public class RPCClient {
+	private static int callID = 0;
+
+	public Session sessionRead(String sessionID, int versionNumber, List<String> ipAddresses) {
+		for (String address : ipAddresses) {
+			try {
+				DatagramSocket socket = new DatagramSocket(RPCConfig.PORT, InetAddress.getByName(address));
+
+				byte[] sendBytes = RPCStream
+						.marshall(String.join(RPCConfig.RPC_DELIMITER, new String[] { String.valueOf(callID),
+								String.valueOf(RPCConfig.READ_CODE), sessionID, String.valueOf(versionNumber) }));
+				DatagramPacket sendPkt = new DatagramPacket(sendBytes, sendBytes.length);
+				socket.send(sendPkt);
+
+				byte[] inBuf = new byte[RPCConfig.MAX_PACKET_LENGTH];
+				DatagramPacket recvPkt = new DatagramPacket(inBuf, inBuf.length);
+
+				String[] recvInfo = null;
+				try {
+					do {
+						recvPkt.setLength(inBuf.length);
+						socket.receive(recvPkt);
+
+						recvInfo = RPCStream.unmarshall(recvPkt.getData()).split(RPCConfig.RPC_DELIMITER);
+					} while (recvPkt.getAddress().getHostAddress().compareTo(address) != 0
+							|| Integer.parseInt(recvInfo[0]) != callID || Integer.parseInt(recvInfo[1]) != 200);
+				} catch (SocketTimeoutException e) {
+					System.out.println("Socket Timeout: " + address);
+					recvPkt = null;
+					socket.close();
+					continue;
+				}
+
+				socket.close();
+				return Session.decode(recvInfo[2]);
+			} catch (IOException e) {
+				System.out.println("IOException in communicating with address: " + address);
+				e.printStackTrace();
+			}
+		}
+
+		callID++;
+		return null;
+	}
 }
