8ec56fda6063282d3863b9a515de6c823ea8a8ea
girikuncoro <girikuncoro@gmail.com>
2016-04-06 02:33:13 -0400

    Tested install script with single instance EC2

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ DB_DOMAIN="LSI"
 N_INSTANCE=1
 
 # file name to store ipAddress-svrID pairs in file system
-INSTANCE_FILE="instances.txt"
+INSTANCE_FILE=instances.txt
 
 # AWS credentials to connect with aws cli
 # It's actually dangerous to store credentials in Github
@@ -21,11 +21,28 @@ INSTANCE_FILE="instances.txt"
 AWS_KEY="AKIAI4FQXGCVF2BFTXYQ"
 AWS_SECRET="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"
 
-# exit if any line fails, and print each cmd executed
+# print each cmd executed
 set -ex
 
-# install tomcat
-yum -y install tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps
+##### IGNORE BELOW, Tomcat8 supports > Java7
+# install java8 to be compatible with tomcat8
+# assume java7 is preconfigured in EC2
+# JAVA_VER=$(java -version 2>&1 | sed 's/.*version "\(.*\)\.\(.*\)\..*"/\1\2/; 1q')
+# if [[ "$JAVA_VER" -eq "18" ]]
+# then
+#     echo "Java8 is already pre-installed"
+# elif [[ "$JAVA_VER" -eq "17" ]]
+# then
+#     echo "Installing Java8 then removing Java7"
+#     sudo yum -y install java-1.8.0
+#     sudo yum -y remove java-1.7.0-openjdk
+# else
+#     echo "Installing Java8"
+#     sudo yum -y install java-1.8.0
+# fi
+
+# install tomcat8
+sudo yum -y install tomcat8-webapps tomcat8-docs-webapp tomcat8-admin-webapps
 
 # aws config credentials, enable simpleDB
 aws configure set aws_access_key_id $AWS_KEY
@@ -34,7 +51,8 @@ aws configure set default.region us-east-1
 aws configure set preview.sdb true
 
 # install app code
-aws s3 cp s3://$S3_BUCKET/$WAR_FILE ~
+S3_URL="$S3_BUCKET/$WAR_FILE"
+aws s3 cp s3://$S3_URL ~
 sudo cp ~/$WAR_FILE /usr/share/tomcat8/webapps
 
 # determine internal IP of this instance, save it to file
@@ -53,7 +71,7 @@ aws sdb put-attributes --domain-name $DB_DOMAIN --item-name `cat ~/local-ipv4` \
 
 # wait for all instances to write
 CURR=0
-while [ $CURR -ne $N_INSTANCE ]
+while [ $CURR -lt $N_INSTANCE ]
 do
     CURR="$(aws sdb select --select-expression "SELECT COUNT(*) FROM $DB_DOMAIN" --output text | grep -o '[0-9]*')"
 
@@ -62,8 +80,8 @@ do
 done
 
 # write all ipAddress from simpleDB to file
-aws sdb select --select-expression "SELECT * FROM $DB_DOMAIN" --output text | grep -v "ITEMS" > file.txt
-sed -i 's/ATTRIBUTES//g; s/^[ \t]*//' file.txt
+aws sdb select --select-expression "SELECT * FROM $DB_DOMAIN" --output text | grep -v "ITEMS" > $INSTANCE_FILE
+sed -i 's/ATTRIBUTES//g; s/^[ \t]*//' $INSTANCE_FILE
 
 # start tomcat
 sudo service tomcat8 start
